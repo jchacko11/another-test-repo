210e9e398a64f58a7402e3ee21f19c4d172af5c1
jchacko11 <jonathan.j.chacko@gmail.com>
2020-01-25 18:11:47 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -15,6 +15,29 @@
 # [START gae_python37_app]
 from flask import Flask
 
+users = {}
+def addUser(userId, first, last, status, target, assassin):
+    users[userId] = [first, last, status, target, assassin]
+
+def getTarget(userId):
+    return users[userId][3]
+
+def getName(userId):
+    return users[userId][0] + " " + users[userId][1]
+
+def getStatus(userId):
+    return users[userId][2]
+
+def getAssassin(userId):
+    return users[userId][4]
+
+addUser(2321, "Anna", "Kolodziejcyk", "Active", "Lauren", "Jonathan")
+print(users)
+print(getTarget(2321))
+print(getName(2321))
+print(getStatus(2321))
+print(getAssassin(2321))
+
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -29,6 +52,10 @@ def hello():
 @app.route("/<name>")              # at the end point /<name>
 def hello_name(name):              # call method hello_name
     return "Hello "+ name          # which returns "hello + name
+
+@app.route("/users", methods=["GET"])
+def list_todo():
+    return jsonify(users)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
