0fc466923dfad8f42ca5e2bfa1eecfd11a3eab95
jchacko11 <jonathan.j.chacko@gmail.com>
2020-01-25 18:09:44 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route("/<name>")              # at the end point /<name>
+def hello_name(name):              # call method hello_name
+    return "Hello "+ name          # which returns "hello + name
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
